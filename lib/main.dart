327b92f48811c4124ed5fcaaa933787623aabbd7
qendev <qendev00@gmail.com>
2021-02-12 18:43:53 +0300

    worked on the amount part

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mpesa_flutter_plugin/mpesa_flutter_plugin.dart';
+import 'package:flutter/services.dart';
 
 //add Dart Async to the imports since this is an asynchronous process
 import 'dart:async';
@@ -17,33 +18,39 @@ class DarajaApp extends StatefulWidget {
 }
 
 class _DarajaAppState extends State<DarajaApp> {
+  final TextEditingController phonenumberController = new TextEditingController();
+  final TextEditingController amountController = new TextEditingController();
+  bool validator = false;
+
+
 
   //create the lipaNaMpesa method here.Please note, the method can have any name, I chose lipaNaMpesa
   Future<void> lipaNaMpesa() async {
     dynamic transactionInitialisation;
     try {
-      transactionInitialisation = await MpesaFlutterPlugin.initializeMpesaSTKPush(
-          businessShortCode: "174379",
-          transactionType: TransactionType.CustomerPayBillOnline,
-          amount: 1.0,
-          partyA:  "254727916009",
-          partyB: "174379",
+      transactionInitialisation =
+          await MpesaFlutterPlugin.initializeMpesaSTKPush(
+              businessShortCode: "174379",
+              transactionType: TransactionType.CustomerPayBillOnline,
+              amount: double.parse(amountController.text),
+              partyA: phonenumberController.text,
+              partyB: "174379",
 //Lipa na Mpesa Online ShortCode
-          callBackURL: Uri(scheme: "https",
-              host: "mpesa-requestbin.herokuapp.com",
-              path: "/1hhy6391"),
+              callBackURL: Uri(
+                  scheme: "https",
+                  host: "mpesa-requestbin.herokuapp.com",
+                  path: "/1hhy6391"),
 //This url has been generated from http://mpesa-requestbin.herokuapp.com/?ref=hackernoon.com for test purposes
-          accountReference: "George Waweru",
-          phoneNumber:  "254727916009",
-          baseUri: Uri(scheme: "https", host: "sandbox.safaricom.co.ke"),
-          transactionDesc: "purchase",
-          passKey: "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919");
+              accountReference: "Cdan",
+              phoneNumber: phonenumberController.text,
+              baseUri: Uri(scheme: "https", host: "sandbox.safaricom.co.ke"),
+              transactionDesc: "purchase",
+              passKey:
+                  "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919");
 //This passkey has been generated from Test Credentials from Safaricom Portal
 
       return transactionInitialisation;
-    }
-
-    catch (e) {
+    } catch (e) {
       print("CAUGHT EXCEPTION: " + e.toString());
     }
   }
@@ -59,21 +66,73 @@ class _DarajaAppState extends State<DarajaApp> {
           centerTitle: true,
         ),
         body: Center(
-          child: RaisedButton(
-            color: Color(0xFF481E4D),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            onPressed: () {
-              lipaNaMpesa();
-            },
-            child: Text(
-              "Lipa na Mpesa",
-              style: TextStyle(color: Colors.white),
+          child: Container(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: phonenumberController,
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter Your PhoneNumber';
+                      }
+                      return null;
+                    },
+
+                    decoration:
+                        InputDecoration(labelText: 'Enter your PhoneNumber'),
+                    keyboardType: TextInputType.phone,
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter.digitsOnly
+                    ],
+                    // inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                    // inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  ),
+                     TextFormField(
+                      controller: amountController,
+                     // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter Your PhoneNumber';
+                        }
+                        return null;
+                      },
+
+                      decoration: InputDecoration(labelText: 'Enter Amount'),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        WhitelistingTextInputFormatter.digitsOnly
+                      ],
+                      // inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                      // inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                    ),
+
+                  SizedBox(
+                    height: 56.0,
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 16.0),
+                      child: RaisedButton(
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        color: Color(0xFF481E4D),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          lipaNaMpesa();
+                        },
+                        child: Text(
+                          "Lipa na Mpesa",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 
+  }
 }
+
